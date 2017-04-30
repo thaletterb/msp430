2bf661f6bc45d5ce701a263ac1af3c88e043f1c3
thaletterb <brian.vuong.87@gmail.com>
2017-04-30 00:10:57 -0700

    Add first MSP430 CCS project

---
+++ MSP430_Blink_LED/Debug/blink.d
@@ -0,0 +1,15 @@
+# FIXED
+
+blink.obj: ../blink.c
+blink.obj: /Applications/ti/ccsv7/ccs_base/msp430/include/msp430.h
+blink.obj: /Applications/ti/ccsv7/ccs_base/msp430/include/msp430f5529.h
+blink.obj: /Applications/ti/ccsv7/ccs_base/msp430/include/in430.h
+blink.obj: /Applications/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics.h
+blink.obj: /Applications/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics_legacy_undefs.h
+
+../blink.c: 
+/Applications/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
+/Applications/ti/ccsv7/ccs_base/msp430/include/msp430f5529.h: 
+/Applications/ti/ccsv7/ccs_base/msp430/include/in430.h: 
+/Applications/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics.h: 
+/Applications/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics_legacy_undefs.h: 
